31857270ffcdad94ddfd7723faaabaa0adab2075
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-08 17:22:22 -0700

    implement PhoneCall

--- IntroGates.kt
+++ IntroGates.kt
@@ -76,7 +76,95 @@ class IntroGates {
 	    
 	}
 
-	
+	/**
+	 * Returns a largest integer n such that n is divisible by divisor
+	 * and less than or equal to the bound. The function will subtract the remainder
+	 * of bound divided by the divisor from the bound to reach the largest n
+	 *
+	 * Problem #5
+	 *
+	 * @author Carlos L. Cuenca
+	 * @date 07/08/2020
+	 * @param int divisor The multiple of n
+	 * @param int bound The highest n  can reach
+	 */
+
+	fun maxMultiple(divisor: Int, bound: Int): Int {
+
+		return bound - (bound % divisor).toInt()
+
+	}
+
+	/**
+	 * Given the amount of numbers in a circle, n, and the firstNumber
+	 * this function will return the radially opposite number from firstNumber
+	 * within the given range of numbers.
+	 * Since there are some radially opposite numbers that are less than 
+	 * the given firstNumber this function takes into account wrap-around
+	 *
+	 * Problem #6
+	 *
+	 * @author Carlos L. Cuenca
+	 * @date 7/08/2020
+	 * @param int n The amount of numbers in the circle
+	 * @param int firstNumber The number that is the point of reference
+	 * @return int The radially opposite number from firstNumber
+	 */
+
+	fun circleOfNumbers(n: Int, firstNumber: Int): Int  {
+
+		return (firstNumber + (n / 2).toInt()) % n
+
+	}
+
+	/**
+	 * Given n minutes, returns the sum of the digits displayed as the time after
+	 * 00:00
+	 * 
+	 * Problem #7
+	 *
+	 * @author: Carlos L. Cuenca
+	 * @since: 07/08/2020
+	 * @param int n The amount of minutes elapsed after 00:00
+	 * @return int the sum of the digits of n minutes ater 00:00
+	 */
+
+	fun lateRide(n: Int): Int {
+
+			return (n/60/10) + (n/60%10) + (n%60/10) + (n%60%10)
+
+	}
+
+	/**
+	 * Given the minute rates min1, min2_10, & min11. This function will return 
+	 * the amount of minutes available from the given cents and the applicable 
+	 * rates.
+	 *
+	 * Problem #8
+	 *
+	 * @author: Carlos L. Cuenca
+	 * @since: 07/08/2020
+	 * @param int min1 The cost of the first minute
+	 * @param int min2_10 The cost of the second to 10th minute
+	 * @param int min11 The cost of the 11th and above minute
+	 * @param int s The amount of available cents
+	 * @return int The amount of minutes given cents, s
+	 */
+
+	fun phoneCall(min1: Int, min2_10: Int, min11: Int, s: Int): Int {
+    
+    if((s - min1) < 0) return 0
+    
+    if((s - min1 - 9*min2_10) < 0) {
+        
+        return (s - min1) / min2_10 + 1
+        
+    }
+    
+    return (s - min1 - 9*min2_10) / min11 + 10
+    
+}
+
 
 
 }
